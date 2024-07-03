60d02eb352c190717e12f9e7b8f5963b2946a230
sophygri <110486850+sophygri@users.noreply.github.com>
2024-07-03 11:54:56 -0300

    M

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-return {"Hello": "World"}
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
-return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}
