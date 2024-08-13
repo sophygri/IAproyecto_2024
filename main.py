e719fc9527c5d707bf5a0baa37df858a9cb2026c
sophygri <110486850+sophygri@users.noreply.github.com>
2024-08-13 14:56:58 -0300

    feat(model)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
 from typing import Union
+from xgboost import XGBClassifier
+import joblib
+import sklearn
+
+model = joblib.load('best_xgb.pkl')
+print(model)
 
 app = FastAPI()
 # uvicorn.run(app, host="0.0.0.0")
@@ -8,7 +14,6 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
